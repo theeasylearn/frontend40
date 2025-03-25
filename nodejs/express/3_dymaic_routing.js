682e101dc10d757ef9727ba7cebde877e55d2d22
theeasylearn <theeasylearn@gmail.com>
2025-03-25 08:38:54 +0530

    dynamic routing

--- nodejs/express/3_dymaic_routing.js
+++ nodejs/express/3_dymaic_routing.js
@@ -6,18 +6,36 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const products = []; //empty array
+var products = []; //empty array
 const ROUTE = "/product"; //read only variable
 //get 
 //localhost:5000/product 
 // purpose : to fetch all products
-app.get(ROUTE, function (request, response) {
-    response.json(products);
+
+//get 
+//localhost:5000/product/Apple 
+// purpose : to fetch all products of given company
+// In Express.js, you can make a route parameter optional by using a ? after the parameter name. Here's how you can modify your route to make the make parameter optional:
+app.get(ROUTE + "/:make?", function (request, response) {
+    let make = request.params.make; 
+    if(make === undefined) //optional parameter is not supplied
+        response.json(products);
+    else 
+    {
+        //optional parameter is supplied
+        let temp = products.filter((item) => {
+            if(item.company === make)
+                return item;
+        });
+        if(temp.length === 0)
+            response.json([{ error: 'product not found' }]);
+        else 
+            response.json(temp);
+    }
 });
 //post 
 //localhost:5000/product 
 // required input : name, price, company
-
 // purpose : to insert new product 
 app.post(ROUTE, function (request, response) {
     var ProductName = request.body.name;
@@ -25,7 +43,7 @@ app.post(ROUTE, function (request, response) {
     var ProductCompany = request.body.company;
     //check if all inputs are given or not (input validation)
     if (ProductName === undefined || ProductPrice === undefined || ProductCompany === undefined) {
-        response.json([{ error: 'input is missing, kindly given name,price and company' }]);
+        response.json([{ error: 'input is missing, kindly give name,price and company' }]);
     }
     else {
         var newProduct = {
@@ -36,23 +54,64 @@ app.post(ROUTE, function (request, response) {
         products.push(newProduct);
         response.json([{ error: 'no' }, { message: 'product added' }]);
     }
-
 });
-
 //delete 
 //localhost:5000/product 
 // purpose : to delete existing product 
 app.delete(ROUTE, function (request, response) {
-    products.pop(); //remove last element from array/list
-    response.json([{ error: 'no' }, { message: 'product deleted' }]);
+    var ProductName = request.body.name;
+    var isFound = false;
+    var temp = products.map((item) => {
+        if(item.name !== ProductName)
+            return item;
+        else 
+            isFound = true;
+    });
+    if(isFound == true)
+    {
+        products = temp;
+        response.json([{ error: 'no' }, { message: 'product deleted' }]);
+    }
+    else 
+    {
+        response.json([{ error: 'no' }, { message: 'product not found' }]);
+    }
 });
-
-
 //localhost:5000/product 
 // purpose : to update detail of existing product 
 app.put(ROUTE, function (request, response) {
-
-    response.json([{ error: 'no' }, { message: 'product updated' }]);
+    var ProductName = request.body.name;
+    var ProductPrice = request.body.price;
+    var ProductCompany = request.body.company;
+    //check if all inputs are given or not (input validation)
+    if (ProductName === undefined || ProductPrice === undefined || ProductCompany === undefined) {
+        response.json([{ error: 'input is missing, kindly give name,price and company' }]);
+    }
+    else 
+    {
+        let isFound = false;
+        let temp = products.map((item) => {
+            if(item.name === ProductName)
+            {
+                var updatedProduct = {
+                    name: ProductName,
+                    price: ProductPrice,
+                    company: ProductCompany
+                };
+                isFound = true;
+                return updatedProduct;
+            }    
+            else 
+                return item;
+        }); //end of map 
+        if(isFound === true)
+        {
+            products = temp;
+            response.json([{ error: 'no' }, { message: 'product updated' }]);
+        }    
+        else 
+            response.json([{ error: 'no' }, { message: 'product not found' }]);   
+    }
 });
 //create route that accept request for non existing route (404)
 app.all("*", function (request, response) {
