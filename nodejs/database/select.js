5520a121b3e94805f36bc864ba198e5799ccafb9
theeasylearn <theeasylearn@gmail.com>
2025-04-09 08:30:03 +0530

    sql commands

--- nodejs/database/select.js
+++ nodejs/database/select.js
@@ -1,4 +1,20 @@
 var c = require('./connection.js');
-var sql = "insert into person (name,gender,dob,salary) values('Tushar Makwana','1','2000-12-31','25000')";
+var sql = "select id,name,gender,salary,date_format(dob,'%a %d-%m-%Y') as dob from person order by name";
 //run sql command 
-c.con
+c.con.query(sql, function (error, result, fields) {
+    if (error)
+        console.log(error);
+    else {
+        //result is an array that has all data as per select statement
+        let rowCount = result.length; //length returns no of element in result array
+        //display field heading 
+        console.log('-'.repeat(60));
+        console.log('ID'.padEnd(10) +'Name'.padEnd(10) +'Gender'.padEnd(15) +'DOB'.padEnd(15) +'Salary'.padEnd(10));
+        console.log('-'.repeat(60));
+        for (let index = 0; index < rowCount; index++) {
+            let row = result[index]; //copy object at given position in row
+            console.log(String(row.id).padEnd(5), " ", row.name.padEnd(15), " ", String(row.gender).padEnd(5), " ", row.dob.padEnd(15), " ", String(row.salary).padEnd(10));
+        }
+    }
+    c.con.end();
+});
--- nodejs/database/update.js
+++ nodejs/database/update.js
@@ -1,1 +1,15 @@
 var c = require('./connection.js');
+var sql = "update person set name='Hitesh Patel',salary = 99999 where id=5";
+c.con.query(sql,function(error,result){
+    if(error)
+        console.log(error);
+    else 
+    {
+        let count = result.affectedRows; //affectedRows return how many inserted/updated/deleted by sql
+        if(count === 0)
+            console.log('none of the rows updated');
+        else 
+            console.log(`${count} rows updated`);
+    }
+    c.con.end();
+});
