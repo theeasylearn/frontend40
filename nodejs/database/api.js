1055214359fd6df03b0209d08e15105e20ba245e
theeasylearn <theeasylearn@gmail.com>
2025-04-28 09:41:00 +0530

    apis

--- nodejs/database/api.js
+++ nodejs/database/api.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var c = require('./connection.js'); //import connection file 
+var validator = require('validator');
+
 var app = express();
 //require below 2 middleware to accept input submitted by post, put, delete method 
 app.use(express.urlencoded({ extended: true }));
@@ -7,35 +9,45 @@ app.use(express.json());
 
 //create routes 
 //(register) insert new user 
-app.post("/user/register",function(request,response){
-    
-    let sql = "insert into users (email,mobile,password) values('tushar@gmail.com','123123','apple')";
-    c.con.query(sql,function(error,result) ////run sql command 
-    {
-        if(error)
+app.post("/user/register", function (request, response) {
+    //create 3 variables using object destruction technique
+    let { email, mobile, password } = request.body;
+    //check all required input is supplied when api called 
+    if (email === undefined || mobile === undefined || password === undefined) {
+        response.json([{ 'error': 'email, mobile password is required' }])
+    }
+    else {
+        let sql = "insert into users (email,mobile,password) values(?,?,?)";
+        //each question is for input there are 3 question mark so we have to create list with 3 items
+        let data = [email, mobile, password];
+
+        c.con.query(sql, data, function (error, result) ////run sql command 
         {
-            console.log(error);
-            response.json([{'error':'oops something went wrong, contact developer'}])
-        }    
-        else 
-        {
-            response.json([{'error':'no'},{'success':'yes'},{'message':'register successful'}]);
-        }
-    });
+            if (error != null) {
+                console.log(error);
+                response.json([{ 'error': 'oops something went wrong, contact developer' }])
+            }
+            else {
+                response.json([{ 'error': 'no' }, { 'success': 'yes' }, { 'message': 'register successful' }]);
+            }
+        });
+    }
+
+
 });
 
 //login api 
-app.post("/user/login",function(request,response){
+app.post("/user/login", function (request, response) {
 
 });
 
 //change password 
-app.post("/user/change-password",function(request,response){
+app.post("/user/change-password", function (request, response) {
 
 });
 
 //forgot password
-app.post("/user/forgot-password",function(request,response){
+app.post("/user/forgot-password", function (request, response) {
 
 });
 app.listen(5000);
